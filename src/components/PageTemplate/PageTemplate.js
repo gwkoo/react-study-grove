37e34cbc06fb8e9016fce7ee4c770c0a8de2e523
gwKoo <gwkoo@grovesoft.net>
2018-10-16 15:24:59 +0900

    발표용 설명 추가

--- src/components/PageTemplate/PageTemplate.js
+++ src/components/PageTemplate/PageTemplate.js
@@ -16,33 +16,28 @@ export default class PageTemplate extends Component {
             ex) <PageTemplate> aaa </PageTemplate> 을 외부에서 사용시  
             {this.props.children} 으로 children 프로퍼티를 가져올 경우 aaa가 들어 있다.
         */
+        const {children} = this.props;
+
         return (
             <div className={cx('page-template')}>
                 <h1>일정 관리</h1>
                 <div className={cx('content')}>
-                    {this.props.children}
+                    {/* {this.props.children} 을 위처럼 render 영역에서 object로 받아서 사용할 수 있다. 뭐든 편한방식을 사용하면 된다. */}
+                    {children}
                 </div>
             </div>
         );
     }
 }
 
-/*
-// type2 functional component 
-const PageTemplate = ({children}) => (
-    <div className={cx('page-template')}>
-        <h1>일정 관리</h1>
-        <div className={cx('content')}>
-            {children}
-        </div>
-    </div>
-)
-export default PageTemplate;
-*/
+
+/**
+ * 함수형 컴포넌트로 사용 방법
+ */
 
 /*
-// type3 functional component 
-const PageTemplate = ({children}) => {
+ - type 1
+ const PageTemplate = ({children}) => {
     return (
         <div className={cx('page-template')}>
             <h1>일정 관리</h1>
@@ -52,24 +47,21 @@ const PageTemplate = ({children}) => {
         </div>
     )
 }
-
 export default PageTemplate;
-*/
 
-/*
-    type2 와 type3의 차이
+ - type 2
+const PageTemplate = ({children}) => (
+    <div className={cx('page-template')}>
+        <h1>일정 관리</h1>
+        <div className={cx('content')}>
+            {children}
+        </div>
+    </div>
+)
+export default PageTemplate;
 
-    - type 2
-    () => { 
-        return(
 
-        )
-    }
-    - type 3
-    () => (
-
-    )
-
-    return 구간을 바로 작성하여서 축약 할수 있다.
-    render return 만 사용시에는 type3이 간결하니 좋을것으로 판단된다.
+위와 같이 상황에 따라서 return 영역(view) 만을 가지는 경우 축약 할수 있다.
+type 1 : () => { return () }
+type 2 : () => ()
 */
